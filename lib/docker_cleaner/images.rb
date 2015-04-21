28694c22899ea5a60616af7001c5d235a16b7596
Christian Lövsund <christian.lovsund@jeppesen.com>
2015-04-21 12:51:41 +0200

    Fix "undefined local variable or method" error. No need to check Created, since that only says when image was built.

--- lib/docker_cleaner/images.rb
+++ lib/docker_cleaner/images.rb
@@ -20,23 +20,24 @@ module DockerCleaner
     end
 
     def clean_old_images
-      one_week_ago = Time.now.to_i - 7 * 24 * 3600
+#      one_week_ago = Time.now.to_i - 7 * 24 * 3600
       Docker::Image.all.each do |image|
-        if image.info["Created"].to_i < one_week_ago
+#        if image.info["Created"].to_i < one_week_ago
           begin
-            puts "Deleting image #{image.id[0..10]} since was created #{image.info} more than one week ago."
+            puts "Deleting image #{image.id[0..10]}."
+            puts "   Info: #{image.info}"
             puts "   Tags: #{image.info['RepoTags']}"
             image.remove(:force => true)
           rescue Docker::Error::NotFoundError
           rescue Excon::Errors::Conflict => e
-            puts "   Conflict when removing #{i.info['RepoTags'][0]} - ID: #{i.id[0...10]}"
+            puts "   Conflict when removing #{image.info['RepoTags'][0]} - ID: #{image.id[0...10]}"
             puts "   !     #{e.response.body}"
           end
           puts "... Done"
-        else
-          puts "Ignoring image #{image.id[0..10]} since was created less than one week ago."
-          puts "   Tags: #{image.info['RepoTags']}"
-        end
+#        else
+#          puts "Ignoring image #{image.id[0..10]} since was created less than one week ago."
+#          puts "   Tags: #{image.info['RepoTags']}"
+ #       end
       end
     end
   end
