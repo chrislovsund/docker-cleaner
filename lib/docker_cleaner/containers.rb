250160ffdcf379c6a15b9359311b6e76b1cb80f9
Christian Lövsund <chris@lovsund.se>
2015-04-23 16:39:28 +0200

    Do not wait a week for cleaning containers.
    Clean if they are exited.

--- lib/docker_cleaner/containers.rb
+++ lib/docker_cleaner/containers.rb
@@ -1,14 +1,15 @@
 module DockerCleaner
   class Containers
     def run
-      # Remove stopped container which stopped with code '0'
-      one_week_ago = Time.now.to_i - 7 * 24 * 3600
-      Docker::Container.all(all: true).select{ |container|
-        container.info["Status"].include?("Exited (0)") || container.info["Status"].include?("Exited (") && container.info["Created"].to_i < one_week_ago
-      }.each do |container|
-        $stdout.write "Remove #{container.id[0...10]} - #{container.info["Image"]} - #{container.info["Names"][0]}"
-        container.remove
-        $stdout.write "... OK\n"
+      puts "Remove stopped container which stopped with code '0'"
+      Docker::Container.all(all: true).each do |container|
+        if container.info["Status"].include?("Exited (0)") || container.info["Status"].include?("Exited (") 
+          $stdout.write "Remove #{container.id[0...10]} - #{container.info["Image"]} - #{container.info["Names"][0]}"
+          container.remove
+          $stdout.write "... OK\n"
+        else
+          puts "Ignore running #{container.id[0...10]} - #{container.info["Image"]} - #{container.info["Names"][0]}"
+        end
       end
     end
   end
